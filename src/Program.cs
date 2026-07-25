7e4592537e6a7ced95b47231c13a1235803ae370
ainobodirou <denik.lis@gmail.com>
2026-07-25 21:30:21 +0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,17 @@
-class Program
+class ShellProgram
 {
     static void Main()
     {
          Console.Write("$ ");
+         string command = Console.Read();
+         Console.WriteLine("{command}: command not found", command);
+    }
+}
+
+class ErrorMessaage
+{
+    public static void PrintErrorMessage(string message)
+    {
+        Console.Writelline("Invalid Input: " + message);
     }
 }
