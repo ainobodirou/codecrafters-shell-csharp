29295c6a8576cd02bf8a7f226f16f7530abfdf48
ainobodirou <denik.lis@gmail.com>
2026-07-25 21:45:09 +0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -2,8 +2,20 @@ class ShellProgram
 {
     static void Main()
     {
-         Console.Write("$ ");
-         string command = Console.ReadLine();
-         Console.WriteLine($"{command}: command not found");
+         while (true)
+        {
+            Console.Write("$ ");
+            string command = Console.ReadLine();
+            Console.WriteLine($"{command}: command not found");
+        }
     }
+
 }
+
+//REPL - Read Eval Print Loop is interactive loop forming core of the shell
+// Read; Displau a prompt and wait for user input 
+// Eval: parse and executre the command 
+// PrintL display output or error message 
+// Loop: return to step 1 and wait for next command 
+
+// Cycle is continues indef until shell process is terminated 
